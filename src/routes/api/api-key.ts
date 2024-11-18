0b0c17788c9dc7b920cabadda6dbac5d82b9e63c
Goon <duy@wearetopgroup.com>
2024-11-18 14:43:17 +0700

    fix(api key): new key generating algorithm

--- src/routes/api/api-key.ts
+++ src/routes/api/api-key.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 
 import { validateSession, verifyRequest } from "@/lib/auth";
 import { prisma } from "@/lib/db";
+import { generateRandomApiKey } from "@/lib/utils/string/random";
 
 dayjs.extend(localizedFormat);
 
@@ -45,7 +46,7 @@ apiKeyRouter.post("/", verifyRequest, validateSession, async (req, res) => {
     }
 
     const { name, expiresAt } = createApiKeySchema.parse(req.body);
-    const key = crypto.randomUUID();
+    const key = await generateRandomApiKey();
     const apiKey = await prisma.apiKey.create({
       data: { name: name || "Untitled", key, userId: user.id, expiresAt },
     });
